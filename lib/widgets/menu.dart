f914083b19cc5c3ac7ee885bc71d0ab2afefb3f3
Brian MacPherson <brimac1634@gmail.com>
2020-11-27 19:22:06 +0800

    changed menu functionality

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -27,155 +27,178 @@ class Menu extends StatelessWidget {
     return Container(
       constraints: BoxConstraints(maxWidth: 400.0),
       padding: EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Inhale Time',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Incrementer(
-                  value: pattern.inhale,
-                  onChange: (value) {
-                    onPatternChange(Pattern(
-                        inhale: value,
-                        exhale: pattern.exhale,
-                        inhalePause: pattern.inhalePause,
-                        exhalePause: pattern.exhalePause));
-                  },
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Exhale Time',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Incrementer(
-                  value: pattern.exhale,
-                  onChange: (value) {
-                    onPatternChange(Pattern(
-                        inhale: pattern.inhale,
-                        exhale: value,
-                        inhalePause: pattern.inhalePause,
-                        exhalePause: pattern.exhalePause));
-                  },
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Inhale Pause',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Incrementer(
-                  value: pattern.inhalePause,
-                  onChange: (value) {
-                    onPatternChange(Pattern(
-                        inhale: pattern.inhale,
-                        exhale: pattern.exhale,
-                        inhalePause: value,
-                        exhalePause: pattern.exhalePause));
-                  },
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Exhale Pause',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Incrementer(
-                  value: pattern.exhalePause,
-                  onChange: (value) {
-                    onPatternChange(Pattern(
-                        inhale: pattern.inhale,
-                        exhale: pattern.exhale,
-                        inhalePause: pattern.inhalePause,
-                        exhalePause: value));
-                  },
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Opacity(
-              opacity: hasVibrator ? 1.0 : 0.0,
+      child: Stack(children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      'Vibration',
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
+                  Text(
+                    'Inhale Time',
+                    style: Theme.of(context).textTheme.bodyText1,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: IconButton(
-                      iconSize: 50,
-                      icon: Icon(
-                        BreatheIcons.vibrate,
-                        color:
-                            vibrationEnabled ? Color(0xfffc00a3) : Colors.white,
-                        size: 50,
-                      ),
-                      onPressed: () {
-                        if (!hasVibrator) return;
-                        if (!vibrationEnabled) Vibration.vibrate(duration: 150);
-                        onVibrationChange(!vibrationEnabled);
-                      },
-                    ),
+                  Incrementer(
+                    value: pattern.inhale,
+                    onChange: (value) {
+                      onPatternChange(Pattern(
+                          inhale: value,
+                          exhale: pattern.exhale,
+                          inhalePause: pattern.inhalePause,
+                          exhalePause: pattern.exhalePause));
+                    },
                   )
                 ],
               ),
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          OutlineButton(
-            highlightedBorderColor: const Color(0xfff0dfea),
-            padding:
-                const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10.0),
-            borderSide: BorderSide(
-                color: Colors.white, width: 1, style: BorderStyle.solid),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(6.0)),
-            child: Text('Save', style: Theme.of(context).textTheme.bodyText1),
-            onPressed: () {
-              onSave();
-            },
-          )
-        ],
-      ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Exhale Time',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  Incrementer(
+                    value: pattern.exhale,
+                    onChange: (value) {
+                      onPatternChange(Pattern(
+                          inhale: pattern.inhale,
+                          exhale: value,
+                          inhalePause: pattern.inhalePause,
+                          exhalePause: pattern.exhalePause));
+                    },
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Inhale Pause',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  Incrementer(
+                    value: pattern.inhalePause,
+                    onChange: (value) {
+                      onPatternChange(Pattern(
+                          inhale: pattern.inhale,
+                          exhale: pattern.exhale,
+                          inhalePause: value,
+                          exhalePause: pattern.exhalePause));
+                    },
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Exhale Pause',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  Incrementer(
+                    value: pattern.exhalePause,
+                    onChange: (value) {
+                      onPatternChange(Pattern(
+                          inhale: pattern.inhale,
+                          exhale: pattern.exhale,
+                          inhalePause: pattern.inhalePause,
+                          exhalePause: value));
+                    },
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Opacity(
+                opacity: hasVibrator ? 1.0 : 0.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        'Vibration',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: Icon(
+                          BreatheIcons.vibrate,
+                          color: vibrationEnabled
+                              ? Color(0xfffc00a3)
+                              : Colors.white,
+                          size: 50,
+                        ),
+                        onPressed: () {
+                          if (!hasVibrator) return;
+                          if (!vibrationEnabled)
+                            Vibration.vibrate(duration: 150);
+                          onVibrationChange(!vibrationEnabled);
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            OutlineButton(
+              highlightedBorderColor: const Color(0xfff0dfea),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10.0),
+              borderSide: BorderSide(
+                  color: Colors.white, width: 1, style: BorderStyle.solid),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6.0)),
+              child:
+                  Text('Reset', style: Theme.of(context).textTheme.bodyText1),
+              onPressed: () {
+                onPatternChange(Pattern(
+                    inhale: 5.5, exhale: 5.5, inhalePause: 0, exhalePause: 0));
+                onVibrationChange(false);
+              },
+            ),
+          ],
+        ),
+        Positioned(
+            top: 0,
+            left: 0,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 40.0,
+                ),
+                onPressed: () {
+                  onSave();
+                },
+              ),
+            ))
+      ]),
     );
   }
 }
